a059d4191defbb6d892ebc7f89920b39d6721679
Ethan <eaj1601@mavs.uta.edu>
2021-11-08 10:30:16 -0600

    Load

---
+++ cse1325_/P09/full_credit/store/Person.java
@@ -0,0 +1,16 @@
+class Person{
+    protected String name;
+    protected String phone;
+    
+    public Person(String name, String phone){
+    
+    }
+    
+    public Person(){
+    
+    }
+    
+    //public save()
+    
+    //String toString()
+}
